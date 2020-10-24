39fbbed89facb360c31d1658ab2c970dca97ea59
sojara <anime.mxa@gmail.com>
2020-10-23 20:50:50 -0700

    Agregado pantallas de funciones para el perfil de Mesero

--- app/src/main/java/com/sojara/oasisfamiliar/MainActivity.java
+++ app/src/main/java/com/sojara/oasisfamiliar/MainActivity.java
@@ -2,7 +2,10 @@ package com.sojara.oasisfamiliar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //botones
+        Button log = (Button)findViewById(R.id.btnLogin);
+        Button menu = (Button)findViewById(R.id.btnMenu);
+
+        //set process to buttons
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        });
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Menu.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/sojara/oasisfamiliar/Meseros_principal.java
+++ app/src/main/java/com/sojara/oasisfamiliar/Meseros_principal.java
@@ -2,7 +2,10 @@ package com.sojara.oasisfamiliar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Meseros_principal extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class Meseros_principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meseros_principal);
+
+        //botones
+        Button mesas = (Button)findViewById(R.id.btnMesas);
+        Button menu = (Button)findViewById(R.id.btnMenu);
+
+        //set process to buttons
+        mesas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Meseros_principal.this, Meseros_mesas.class));
+            }
+        });
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Meseros_principal.this, Menu.class));
+            }
+        });
     }
 }
